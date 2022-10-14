dff40ffea6853ca3d05e45c4ca865aa6cdf7eb12
Gabriela <gabrielacamarchiori@gmail.com>
2022-10-14 09:40:21 -0300

    add protected routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Dashboard from "../pages/dashboard";
 import Signup from "../pages/signup";
 import Login from "../pages/login";
+import ProtectedRoutes from "../components/ProtectedRoutes";
 
 const ToAuthenticate = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route element={<ProtectedRoutes/>}>
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 };
